2de52545e829e1613cc0b10d88f846116994d63b
Lovegruru <kambagouniprashanth846@gmail.com>
2023-12-24 08:15:34 +0530

    First commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,29 +2,27 @@ import React,{useEffect, useState} from "react";
 
 const Login=()=>{
     const[user,setUser]=useState([]);
+
     useEffect(()=>{
         let user=JSON.parse(localStorage.getItem("user"))
-        setUser(JSON.parse(localStorage.getItem("user")))
-        console.log(user)
-        // localStorage.removeItem("user");
-        
-
+        setUser(user)
     },[])
-   
+
+    const renderUser = (user) => {
+        return (
+            <div>
+                <h1>Logged In</h1>
+                <h4>Id:{user.id}</h4>
+                <h4>FirstName:{user.firstName}</h4>
+                <h4>LastName:{user.lastName}</h4>
+                <h4>Gender:{user.gender}</h4>
+            </div>
+        )
+    }
+
     return(
         <div className="Login">
-
-            <h1>Logged In</h1>
-            <h4>Id:{user.id}</h4>
-            <h4>FirstName:{user.firstName}</h4>
-            <h4>LastName:{user.lastName}</h4>
-            <h4>Gender:{user.gender}</h4>
-
-
-
-
-
-
+           {user && user.length > 0 ? user.map(renderUser) : renderUser(user)}
         </div>
     )
 }
